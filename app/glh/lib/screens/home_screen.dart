51bc37cb4407200cd98188fe3d23074fe6abc39b
Asti7 <astitvagautam@gmail.com>
2020-05-11 23:48:58 +1000

    home_screen (main page) for app started.

---
+++ app/glh/lib/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+static const String id = 'home_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
